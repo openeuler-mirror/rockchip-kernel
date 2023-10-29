36cac1c8a50b6cf6d29a492498eefc90e4025151
YuzukiTsuru <gloomyghost@gloomyghost.com>
2023-10-29 18:14:55 +0800

    merge driver gpio with rockchip platform

--- drivers/gpio/gpio-mockup.c
+++ drivers/gpio/gpio-mockup.c
@@ -375,13 +375,6 @@ static void gpio_mockup_debugfs_setup(struct device *dev,
 	}
 }
 
-static void gpio_mockup_debugfs_cleanup(void *data)
-{
-	struct gpio_mockup_chip *chip = data;
-
-	debugfs_remove_recursive(chip->dbg_dir);
-}
-
 static void gpio_mockup_dispose_mappings(void *data)
 {
 	struct gpio_mockup_chip *chip = data;
@@ -464,7 +457,7 @@ static int gpio_mockup_probe(struct platform_device *pdev)
 
 	gpio_mockup_debugfs_setup(dev, chip);
 
-	return devm_add_action_or_reset(dev, gpio_mockup_debugfs_cleanup, chip);
+	return 0;
 }
 
 static struct platform_driver gpio_mockup_driver = {
@@ -604,9 +597,9 @@ static int __init gpio_mockup_init(void)
 
 static void __exit gpio_mockup_exit(void)
 {
-	gpio_mockup_unregister_pdevs();
 	debugfs_remove_recursive(gpio_mockup_dbg_dir);
 	platform_driver_unregister(&gpio_mockup_driver);
+	gpio_mockup_unregister_pdevs();
 }
 
 module_init(gpio_mockup_init);
