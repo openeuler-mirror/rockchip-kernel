ccea8b15151779c9ca25d7fbead40924e3166172
YuzukiTsuru <gloomyghost@gloomyghost.com>
2023-10-29 18:25:04 +0800

    merge net driver sctp with rockchip platform

--- net/sctp/socket.c
+++ net/sctp/socket.c
@@ -411,6 +411,9 @@ static int sctp_do_bind(struct sock *sk, union sctp_addr *addr, int len)
 		}
 	}
 
+	if (snum && inet_is_local_unbindable_port(net, snum))
+		return -EPERM;
+
 	if (snum && inet_port_requires_bind_service(net, snum) &&
 	    !ns_capable(net->user_ns, CAP_NET_BIND_SERVICE))
 		return -EACCES;
@@ -1079,6 +1082,8 @@ static int sctp_connect_new_asoc(struct sctp_endpoint *ep,
 		if (sctp_autobind(sk))
 			return -EAGAIN;
 	} else {
+		if (inet_is_local_unbindable_port(net, ep->base.bind_addr.port))
+			return -EPERM;
 		if (inet_port_requires_bind_service(net, ep->base.bind_addr.port) &&
 		    !ns_capable(net->user_ns, CAP_NET_BIND_SERVICE))
 			return -EACCES;
@@ -4991,17 +4996,13 @@ static void sctp_destroy_sock(struct sock *sk)
 }
 
 /* Triggered when there are no references on the socket anymore */
-static void sctp_destruct_common(struct sock *sk)
+static void sctp_destruct_sock(struct sock *sk)
 {
 	struct sctp_sock *sp = sctp_sk(sk);
 
 	/* Free up the HMAC transform. */
 	crypto_free_shash(sp->hmac);
-}
 
-static void sctp_destruct_sock(struct sock *sk)
-{
-	sctp_destruct_common(sk);
 	inet_sock_destruct(sk);
 }
 
@@ -5227,12 +5228,11 @@ int sctp_transport_lookup_process(int (*cb)(struct sctp_transport *, void *),
 }
 EXPORT_SYMBOL_GPL(sctp_transport_lookup_process);
 
-int sctp_transport_traverse_process(sctp_callback_t cb, sctp_callback_t cb_done,
-				    struct net *net, int *pos, void *p)
-{
+int sctp_for_each_transport(int (*cb)(struct sctp_transport *, void *),
+			    int (*cb_done)(struct sctp_transport *, void *),
+			    struct net *net, int *pos, void *p) {
 	struct rhashtable_iter hti;
 	struct sctp_transport *tsp;
-	struct sctp_endpoint *ep;
 	int ret;
 
 again:
@@ -5241,32 +5241,26 @@ again:
 
 	tsp = sctp_transport_get_idx(net, &hti, *pos + 1);
 	for (; !IS_ERR_OR_NULL(tsp); tsp = sctp_transport_get_next(net, &hti)) {
-		ep = tsp->asoc->ep;
-		if (sctp_endpoint_hold(ep)) { /* asoc can be peeled off */
-			ret = cb(ep, tsp, p);
-			if (ret)
-				break;
-			sctp_endpoint_put(ep);
-		}
+		ret = cb(tsp, p);
+		if (ret)
+			break;
 		(*pos)++;
 		sctp_transport_put(tsp);
 	}
 	sctp_transport_walk_stop(&hti);
 
 	if (ret) {
-		if (cb_done && !cb_done(ep, tsp, p)) {
+		if (cb_done && !cb_done(tsp, p)) {
 			(*pos)++;
-			sctp_endpoint_put(ep);
 			sctp_transport_put(tsp);
 			goto again;
 		}
-		sctp_endpoint_put(ep);
 		sctp_transport_put(tsp);
 	}
 
 	return ret;
 }
-EXPORT_SYMBOL_GPL(sctp_transport_traverse_process);
+EXPORT_SYMBOL_GPL(sctp_for_each_transport);
 
 /* 7.2.1 Association Status (SCTP_STATUS)
 
@@ -5522,7 +5516,7 @@ int sctp_do_peeloff(struct sock *sk, sctp_assoc_t id, struct socket **sockp)
 	 * Set the daddr and initialize id to something more random and also
 	 * copy over any ip options.
 	 */
-	sp->pf->to_sk_daddr(&asoc->peer.primary_addr, sock->sk);
+	sp->pf->to_sk_daddr(&asoc->peer.primary_addr, sk);
 	sp->pf->copy_ip_options(sk, sock->sk);
 
 	/* Populate the fields of the newsk from the oldsk and migrate the
@@ -9195,7 +9189,7 @@ void sctp_copy_sock(struct sock *newsk, struct sock *sk,
 	sctp_sk(newsk)->reuse = sp->reuse;
 
 	newsk->sk_shutdown = sk->sk_shutdown;
-	newsk->sk_destruct = sk->sk_destruct;
+	newsk->sk_destruct = sctp_destruct_sock;
 	newsk->sk_family = sk->sk_family;
 	newsk->sk_protocol = IPPROTO_SCTP;
 	newsk->sk_backlog_rcv = sk->sk_prot->backlog_rcv;
@@ -9427,20 +9421,11 @@ struct proto sctp_prot = {
 
 #if IS_ENABLED(CONFIG_IPV6)
 
-static void sctp_v6_destruct_sock(struct sock *sk)
+#include <net/transp_v6.h>
+static void sctp_v6_destroy_sock(struct sock *sk)
 {
-	sctp_destruct_common(sk);
-	inet6_sock_destruct(sk);
-}
-
-static int sctp_v6_init_sock(struct sock *sk)
-{
-	int ret = sctp_init_sock(sk);
-
-	if (!ret)
-		sk->sk_destruct = sctp_v6_destruct_sock;
-
-	return ret;
+	sctp_destroy_sock(sk);
+	inet6_destroy_sock(sk);
 }
 
 struct proto sctpv6_prot = {
@@ -9450,8 +9435,8 @@ struct proto sctpv6_prot = {
 	.disconnect	= sctp_disconnect,
 	.accept		= sctp_accept,
 	.ioctl		= sctp_ioctl,
-	.init		= sctp_v6_init_sock,
-	.destroy	= sctp_destroy_sock,
+	.init		= sctp_init_sock,
+	.destroy	= sctp_v6_destroy_sock,
 	.shutdown	= sctp_shutdown,
 	.setsockopt	= sctp_setsockopt,
 	.getsockopt	= sctp_getsockopt,
