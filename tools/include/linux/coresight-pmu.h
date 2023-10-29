3a50ebcdc74914bddcb36c1746506359c130ba99
YuzukiTsuru <gloomyghost@gloomyghost.com>
2023-10-29 18:29:16 +0800

    merge tools include with rockchip platform

--- tools/include/linux/coresight-pmu.h
+++ tools/include/linux/coresight-pmu.h
@@ -8,25 +8,19 @@
 #define _LINUX_CORESIGHT_PMU_H
 
 #define CORESIGHT_ETM_PMU_NAME "cs_etm"
-#define CORESIGHT_ETM_PMU_SEED  0x1
-#define CORESIGHT_ETM_CSID_MAX  0x70
+#define CORESIGHT_ETM_PMU_SEED  0x10
 
 /* ETMv3.5/PTM's ETMCR config bit */
-#define ETM_OPT_BRANCH_BROADCAST 8
 #define ETM_OPT_CYCACC  12
 #define ETM_OPT_CTXTID	14
-#define ETM_OPT_CTXTID2	15
 #define ETM_OPT_TS      28
 #define ETM_OPT_RETSTK	29
 
 /* ETMv4 CONFIGR programming bits for the ETM OPTs */
-#define ETM4_CFG_BIT_BB         3
 #define ETM4_CFG_BIT_CYCACC	4
 #define ETM4_CFG_BIT_CTXTID	6
-#define ETM4_CFG_BIT_VMID	7
 #define ETM4_CFG_BIT_TS		11
 #define ETM4_CFG_BIT_RETSTK	12
-#define ETM4_CFG_BIT_VMID_OPT	15
 
 static inline int coresight_get_trace_id(int cpu)
 {
@@ -36,7 +30,7 @@ static inline int coresight_get_trace_id(int cpu)
 	 * the common convention is to have data trace IDs be I(N) + 1,
 	 * set instruction trace IDs as a function of the CPU number.
 	 */
-	return (CORESIGHT_ETM_PMU_SEED + (cpu * 2)) % CORESIGHT_ETM_CSID_MAX;
+	return (CORESIGHT_ETM_PMU_SEED + (cpu * 2));
 }
 
 #endif
